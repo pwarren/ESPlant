c5e35ed523f3d89b962b94158a9dd5fcfc453132
Paul Warren <pwarren@pwarren.id.au>
2016-02-04 22:15:48 +1100

    Proper arduino naming convention, compiles and uploads, think it works but my LED ring is dodgy

--- Firmware/ESPlant_LED_Temp/ESPlant_LED_temp/ESPlant_LED_temp.ino
+++ Firmware/ESPlant_LED_Temp/ESPlant_LED_temp/ESPlant_LED_temp.ino
@@ -9,9 +9,7 @@
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(MAX_LEDS, 13, NEO_GRB + NEO_KHZ800);
 OneWire oneWire(ONE_WIRE_BUS);
-DallasTemperature dallsTemp(&oneWire);
-
-uint16_t curRingLed = 0;
+DallasTemperature dallasTemp(&oneWire);
 
 void setup() {
   pixels.begin();
@@ -21,17 +19,17 @@ void setup() {
 
 void loop() {
   int temp_colour = 0;
+  uint16_t curRingLED = 0;
 
   temp_colour = int(dallasTemp.getTempCByIndex(0));
   // scale range from [5..30] to [0..255]
   temp_colour = (temp_colour - 5) * 8.5;
 
-  for(curRingLed=0; curRingLED < MAX_LEDS; curRingLed++) {
-    pixels.setPixelColor(curRingLed, pixels.Color(temp_colour, 0, 255 - temp_colour));
+  for(curRingLED=0; curRingLED < MAX_LEDS; curRingLED++) {
+    pixels.setPixelColor(curRingLED, pixels.Color(temp_colour, 0, 255 - temp_colour));
   }
-
   pixels.show();
 
-  ESP.deepSleep(SLEEP_MINUTES * 60 * 1000l * 1000l);  
+  delay(1000);
 }
 
